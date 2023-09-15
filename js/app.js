b856f5f001805230228f43601bcb79312af2ff3e
ClaytonMalarkey <malarkey1985@gmail.com>
2023-09-14 19:41:33 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+let item_Names = ["icecream", "chocolate", "stawberries"];
+let item_Prices = [10, 1, 5];
+let item_InStock = [true, true, false];
+let first_item_name = item_Names[0];
+let first_item_price = item_Prices[0];
+let first_item_instock = item_InStock[0];
+item_Names.push["blueberries"];
+item_Prices.push[6];
+item_InStock.push [false];
+let last_item_name = item_Names.pop;
+let last_item_price = item_Prices.pop;
+let last_item_instock = item_InStock.pop;
+
